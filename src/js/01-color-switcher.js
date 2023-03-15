91aaa88f68c303efa65c3867d963db1d1ba0ccab
Shadioso <114300237+Shadioso@users.noreply.github.com>
2023-03-15 20:25:00 +0200

    Draft

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+//
+const body = document.querySelector(`body`);
+const startBtn = document.querySelector(`button[data-start]`);
+const closeBtn = document.querySelector(`button[data-stop]`);
+//
 
+const changeColor = () => {
+  startBtn.disabled = true;
+  timeOutId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+const stopChanheColor = () => {
+  startBtn.disabled = false;
+  clearInterval(timeOutId);
+};
+//
+startBtn.addEventListener(`click`, changeColor);
+closeBtn.addEventListener(`click`, stopChanheColor);
