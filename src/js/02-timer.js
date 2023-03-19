26c121682a22b977689ee9e8a32a63d908e5f125
Shadioso <114300237+Shadioso@users.noreply.github.com>
2023-03-19 12:18:37 +0200

    Finall

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,61 +1,74 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+const input = document.querySelector(`input`);
+const startBtn = document.querySelector(`button[data-start]`);
+const day = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+const dateChosen = document.querySelector('#datetime-picker');
+//
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  timeId: null,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    if (selectedDates[0] <= new Date()) {
+      startBtn.disabled = true;
+      return alert('Please choose a date in the future');
+    } else {
+      startBtn.disabled = false;
+      startBtn.addEventListener(`click`, startTimer);
+      function startTimer() {
+        this.timeId = setInterval(() => {
+          const distance = selectedDates[0] - new Date();
+          const timeComponents = convertMs(distance);
+          updateClockFace(timeComponents);
+          if (distance < 1000) {
+            clearInterval(timeId);
+            startBtn.disabled = false;
+          }
+        }, 1000);
+      }
+    }
   },
 };
-const input = document.querySelector(`input`);
-const startBtn = document.querySelector(`button[data-start]`);
-let dataValue = 0;
-//
-flatpickr(input, options);
 
-const startTimer = () => {
-  isActive = () => !dataValue;
-  if (isActive) {
-    startBtn.disabled = true;
-  }
-  const timer = setInterval(() => {
-    dataValue = timer;
-    const distance = getDistance();
-    if (distance < Date.now().getTime()) {
-      return alert('Please choose a date in the future');
-    } else if (distance > Date.now().getTime()) {
-      startBtn.disabled = false;
-    } else if (distance === 0) {
-      return alert(`the countdown is over`);
-    }
-  }, 1000);
-};
+flatpickr(dateChosen, options);
 
-const getDistance = () => {
-  const currentDate = Date.now().getTime();
-  return selectedDates.getTime() - currentDate;
-};
-startBtn.addEventListener(`click`, startTimer);
+function updateClockFace({
+  days: daysValue,
+  hours: hoursValue,
+  minutes: minutesValue,
+  seconds: secondsValue,
+}) {
+  day.textContent = `${daysValue}`;
+  hours.textContent = `${hoursValue}`;
+  minutes.textContent = `${minutesValue}`;
+  seconds.textContent = `${secondsValue}`;
+}
+
+function makeToZero(value) {
+  return String(value).padStart(2, '0');
+}
+
 //
-//
-const distance = getDistance();
-function convertMs(distance) {
-  // Number of milliseconds per unit of time
+function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = makeToZero(Math.floor(ms / day));
+  const hours = makeToZero(Math.floor((ms % day) / hour));
+  const minutes = makeToZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = makeToZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const firstDelay = document.querySelector(`input[name=delay]`);
+const step = document.querySelector(`input[name=step]`);
+const amount = document.querySelector(`input[name=amount]`);
+const button = document.querySelector(`button`);
+const form = document.querySelector(`form`);
+//
+const createPromise = (position, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
+
+const onSubmitForm = evt => {
+  evt.preventDefault();
+  let firstDelayValue = firstDelay.valueAsNumber;
+  const stepValue = step.valueAsNumber;
+  const amountValue = amount.valueAsNumber;
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    createPromise(i, firstDelayValue)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    firstDelayValue += stepValue;
   }
-}
+};
+//
+form.addEventListener(`submit`, onSubmitForm);
